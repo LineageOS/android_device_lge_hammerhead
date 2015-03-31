89dfd8d2536fd899ef4c469c3dfd96e215a114f9
Mansoor Aftab <maftab@codeaurora.org>
2015-03-31 00:09:41 -0700

    QCamera2: Add HAL3 support for flashlight
    Implemented new API at HAL, allowing framework to turn on flash
    devices independently of the sensor. The primary logic is
    implemented as a singleton named QCameraFlash, which tracks
    the state of any flash devices and interfaces with the driver
    to turn them on or off.
    
    Change-Id: I4452e65401140e28aa86184800857a423c9c4b09
    Adapted to hammerhead camera from CAF via shamu's
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- camera/QCamera2/stack/common/cam_types.h
+++ camera/QCamera2/stack/common/cam_types.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -54,6 +54,8 @@
 #define MAX_AVAILABLE_CAPABILITIES 6
 
 #define GPS_PROCESSING_METHOD_SIZE 33
+
+#define QCAMERA_MAX_FILEPATH_LENGTH 64
 
 typedef enum {
     CAM_HAL_V1 = 1,
