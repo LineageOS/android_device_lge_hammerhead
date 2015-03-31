f08fba34611f26e38fdbc7cc446b57f5d516cc0c
Mansoor Aftab <maftab@codeaurora.org>
2015-03-31 00:09:41 -0700

    QCamera2: Add HAL3 support for flashlight
    Implemented new API at HAL, allowing framework to turn on flash
    devices independently of the sensor. The primary logic is
    implemented as a singleton named QCameraFlash, which tracks
    the state of any flash devices and interfaces with the driver
    to turn them on or off.
    
    Change-Id: I4452e65401140e28aa86184800857a423c9c4b09
    Adapted to hammerhead camera from CAF via shamu's
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- camera/QCamera2/HAL3/QCamera3HWI.h
+++ camera/QCamera2/HAL3/QCamera3HWI.h
@@ -45,6 +45,16 @@ extern "C" {
 #include <mm_jpeg_interface.h>
 }
 
+#ifdef CDBG
+#undef CDBG
+#endif //#ifdef CDBG
+#define CDBG(fmt, args...) ALOGV(fmt, ##args)
+
+#ifdef CDBG_HIGH
+#undef CDBG_HIGH
+#endif //#ifdef CDBG_HIGH
+#define CDBG_HIGH(fmt, args...) ALOGD(fmt, ##args)
+
 using namespace android;
 
 namespace qcamera {
@@ -198,6 +208,9 @@ public:
     int getJpegQuality();
     int calcMaxJpegSize();
     QCamera3Exif *getExifData();
+    static void getFlashInfo(const int cameraId,
+            bool& hasFlash,
+            char (&flashNode)[QCAMERA_MAX_FILEPATH_LENGTH]);
 public:
     static int kMaxInFlight;
 private:
