a87f50fa4c5a9ed165f42b28f37996fc1cf5f09d
Shuzhen Wang <shuzhenw@codeaurora.org>
2015-03-31 22:53:26 -0700

    Camera3: Add support for conflicting_devices and resource_cost
    resource_cost of a particular camera is calculated based on
    overall CPP bandwidth limit and maximum stream configuration
    bandwidth requirement.
    
    Change-Id: Ic207ffe07f51a13acff4c6f9ce7f35d683f12e81
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- camera/QCamera2/stack/common/cam_intf.h
+++ camera/QCamera2/stack/common/cam_intf.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -308,6 +308,9 @@ typedef struct{
     cam_opaque_raw_format_t opaque_raw_fmt;
 
     uint8_t flash_dev_name[QCAMERA_MAX_FILEPATH_LENGTH];
+
+    /* maximum pixel bandwidth shared between cameras */
+    uint64_t max_pixel_bandwidth;
 } cam_capability_t;
 
 typedef enum {
