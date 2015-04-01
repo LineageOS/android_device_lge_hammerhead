d2414b5337555ffa0ce95ba21e7f3ddbca0b6a5a
Shuzhen Wang <shuzhenw@codeaurora.org>
2015-03-31 22:53:26 -0700

    Camera3: Add support for conflicting_devices and resource_cost
    resource_cost of a particular camera is calculated based on
    overall CPP bandwidth limit and maximum stream configuration
    bandwidth requirement.
    
    Change-Id: Ic207ffe07f51a13acff4c6f9ce7f35d683f12e81
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -4649,10 +4649,30 @@ int QCamera3HardwareInterface::getCamInfo(int cameraId,
         break;
     }
 
-
     info->orientation = gCamCapability[cameraId]->sensor_mount_angle;
     info->device_version = CAMERA_DEVICE_API_VERSION_3_2;
     info->static_camera_characteristics = gStaticMetadata[cameraId];
+
+    //For now assume both cameras can operate independently.
+    info->conflicting_devices = NULL;
+    info->conflicting_devices_length = 0;
+
+    //resource cost is 100 * MIN(1.0, m/M),
+    //where m is throughput requirement with maximum stream configuration
+    //and M is CPP maximum throughput.
+    float max_fps = 0.0;
+    for (uint32_t i = 0;
+            i < gCamCapability[cameraId]->fps_ranges_tbl_cnt; i++) {
+        if (max_fps < gCamCapability[cameraId]->fps_ranges_tbl[i].max_fps)
+            max_fps = gCamCapability[cameraId]->fps_ranges_tbl[i].max_fps;
+    }
+    float ratio = 1.0 * MAX_PROCESSED_STREAMS *
+            gCamCapability[cameraId]->active_array_size.width *
+            gCamCapability[cameraId]->active_array_size.height * max_fps /
+            gCamCapability[cameraId]->max_pixel_bandwidth;
+    info->resource_cost = 100 * MIN(1.0, ratio);
+    ALOGI("%s: camera %d resource cost is %d", __func__, cameraId,
+            info->resource_cost);
 
     return rc;
 }
