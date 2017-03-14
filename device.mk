43702480c440a7dcd73b26ce75035b9b71d4f5ac
Janis Danisevskis <jdanis@google.com>
2017-03-14 16:26:00 -0700

    device/manifest: Make keymaster binderized always
    Bug: 35764921
    Test: Boot the device and check the output of lshal. The keymaster
          service must be running and should have two clients: vold and
          keystore
    
    Change-Id: I62c78ec17541675027204aeacb9fe0aa1c9df1f0
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- device.mk
+++ device.mk
@@ -189,6 +189,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service \
     keystore.msm8974
 
 # GPS configuration
