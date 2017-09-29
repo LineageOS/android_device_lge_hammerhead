679df5430372d2670b9628010ff6bd0b100b23da
D. Andrei Măceș <dmaces@nd.edu>
2017-09-29 19:08:38 +0300

    device: Add audio, lights, sensors, etc. HIDL implementations
    The following passthrough HIDL HALs are added initially:
    - Audio
    - DRM
    - Keymaster
    - NFC (NCI)
    - Power
    - Sensors
    - Thermal
    - Vibrator
    
    Change-Id: Ib10e97542a4a36d741a0b85572a454c3101cd80c
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- device.mk
+++ device.mk
@@ -153,6 +153,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.media.treble_omx=false
 
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
     audio.primary.msm8974 \
     audio.a2dp.default \
     audio.usb.default \
@@ -162,6 +163,7 @@ PRODUCT_PACKAGES += \
 
 # Audio effects
 PRODUCT_PACKAGES += \
+    android.hardware.audio.effect@2.0-impl \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcomvoiceprocessingdescriptors \
@@ -182,6 +184,7 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
     keystore.msm8974
 
 PRODUCT_PACKAGES += \
@@ -199,14 +202,20 @@ PRODUCT_PACKAGES += \
 
 # NFC packages
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl-bcm \
     nfc_nci.bcm2079x.default \
     NfcNci \
     Tag
+
+# Vibrator HAL
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
 
 PRODUCT_PACKAGES += \
     libion
 
 PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
     lights.hammerhead
 
 PRODUCT_PACKAGES += \
@@ -220,7 +229,12 @@ PRODUCT_PACKAGES += \
     bdAddrLoader
 
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
     power.hammerhead
+
+# Thermal HAL
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
@@ -247,6 +261,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
+
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
 
 # Set sensor streaming rate
 PRODUCT_PROPERTY_OVERRIDES += \
