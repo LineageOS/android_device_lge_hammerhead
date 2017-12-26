28315d375a5ce3e5560629cd163c052f6577714f
kazu <tdukazudon@gmail.com>
2017-12-26 16:57:11 +0900

    hammerhead: Build bcm nfc implementation
    Change-Id: I7bab2185bf53fcd6ba88cef97e1b0794a06398e0

--- device.mk
+++ device.mk
@@ -203,6 +203,11 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag
 
+# NFC hal interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl-bcm \
+    android.hardware.nfc@1.0-service
+
 PRODUCT_PACKAGES += \
     libion
 
