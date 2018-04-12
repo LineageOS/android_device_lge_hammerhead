14410ce656753d631d5a2962cae59fc33ce80491
Krizthian <cristianortizmorales2013@gmail.com>
2018-04-12 06:49:29 -0500

    hammerhead: Build LiveDisplay HAL
    Change-Id: If99fc58e102ec151dd82be667915355f9203d79c

--- device.mk
+++ device.mk
@@ -140,6 +140,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+#LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
+
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libstagefrighthw \
