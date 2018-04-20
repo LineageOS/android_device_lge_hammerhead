869319b3c9167a905f4da53fc023473d976d8388
D. Andrei Măceș <andrei@unlegacy-android.org>
2018-04-20 15:26:30 +0300

    hammerhead: camera: Also prepare kernel headers in mm-jpeg-interface
    This fixup only became apparent after the removal of legacy headers.
    
    Change-Id: I0ddb2074ae20ab32bc5a6a7ce80542b555c18d1f
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- camera/QCamera2/stack/mm-jpeg-interface/Android.mk
+++ camera/QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -12,6 +12,9 @@ LOCAL_C_INCLUDES += \
     $(LOCAL_PATH)/../../../mm-image-codec/qexif \
     $(LOCAL_PATH)/../../../mm-image-codec/qomx_core
 
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
 ifeq ($(strip $(TARGET_USES_ION)),true)
     LOCAL_CFLAGS += -DUSE_ION
 endif
