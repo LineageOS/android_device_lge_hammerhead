b5b889b46361dd87e676222e811bda85f2fd7925
Tim Clark <tim.clark.82@gmail.com>
2018-06-26 12:12:58 -0700

    hammerhead: Drop deprecated HWUI props
    These props have been deprecated in Oreo.
    We are not calling the make files that are being removed from
    frameworks/native, but we might as well remove these props.
    
     * In Android 8.1 and later, only the ro.zygote.disable_gl_preload
       property still applies. All other properties have been removed.
       Reference: https://source.android.com/devices/graphics/renderer
    
    Change-Id: I1cd84c1b4cff8e99406e6eb6be6003806facbf14

--- device.mk
+++ device.mk
@@ -251,18 +251,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.hwui.use_buffer_age=false \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
+    debug.hwui.use_buffer_age=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
