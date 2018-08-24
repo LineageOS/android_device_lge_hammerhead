663c3f488cf0aa40bf5f62e07ff407dd55aeca23
Danny Baumann <dannybaumann@web.de>
2018-08-24 09:01:55 +0200

    hammerhead: Consider lineage_hammerhead product via the AOSP way
    Change-Id: I085b2a6f93b5e70c38281038b8a83b467824eda1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2013 The Android Open Source Project
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,5 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_hammerhead.mk \
-    $(LOCAL_DIR)/full_hammerhead.mk
+    $(LOCAL_DIR)/lineage_hammerhead.mk
