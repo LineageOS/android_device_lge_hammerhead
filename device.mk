512a72b7a7083ba9fddcb608c99bee81fbed40a4
z3DD3r <z3dd3r@gmail.com>
2019-08-03 20:23:10 +0300

    hammerhead: Add privapp permissions
    * https://source.android.com/devices/tech/config/perms-whitelist#generating-whitelists
    
    Change-Id: I2cedbd235fe037859ead9ae31d67f91bf6e5cbc6

--- device.mk
+++ device.mk
@@ -105,6 +105,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/lge/hammerhead/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
 
+# Privapp Whitelist
+PRODUCT_COPY_FILES += \
+    device/lge/hammerhead/permissions/privapp-permissions-hammerhead.xml:system/etc/permissions/privapp-permissions-hammerhead.xml
+
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
