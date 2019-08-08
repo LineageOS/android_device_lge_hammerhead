63763855da5b9c176eb1c4dea77c8dc61cc78a2c
Nolen Johnson <johnsonnolen@gmail.com>
2019-08-07 21:16:11 -0400

    hammerhead: Remove {aosp|full}_hammerhead tagets
    * We don't support building AOSP with this tree anyway,
      and don't declare them in AndroidProducts.
    * Integrate what's needed into the lineage_hammerhead target.
    
    Change-Id: Ied159871b32568f6c51a89ace9bdfc2964c44760

--- lineage_hammerhead.mk
+++ lineage_hammerhead.mk
@@ -3,11 +3,17 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
+# Inherit proprietary-files
+$(call inherit-product-if-exists, vendor/lge/hammerhead/hammerhead-vendor.mk)
+
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Inherit device configuration
-$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
+$(call inherit-product, device/lge/hammerhead/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hammerhead
@@ -15,6 +21,7 @@ PRODUCT_NAME := lineage_hammerhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
